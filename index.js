b5c3ced9de0b4853d4909cbe65e741bfce96aa92
asiedusolo <solomon.asiedu@amalitech.org>
2022-02-01 12:38:30 +0000

    adding server port from env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const connectDB = require("./db/connect");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
